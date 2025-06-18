ffd8638a9f8c9da5fb3b20aa34a17e41e5e3ae36
Jamari McFarlane <jamarimcfarlane12@gmail.com>
2025-06-17 19:37:31 -0500

    refactor:adjust spacing between lines in App.js

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,6 @@ import EvacuationRoutesC from "./features/evacuation-routes";
 function App() {
   return (
     <Router>
-      {/* <div className="h-[80vh] w-[70vw] border border-red-500 m-auto p-4 space-y-4"> */}
-
         <nav className="flex gap-4 text-sm font-medium border-b pb-2">
           <Link to="/" className="hover:text-blue-600">Firebreaks</Link>
           <Link to="/spread" className="hover:text-blue-600">Fire Spread</Link>
@@ -19,7 +17,6 @@ function App() {
           <Route path="/spread" element={<FireSpreadSimulation />} />
           <Route path="/evacuation" element={<EvacuationRoutesC />} />
         </Routes>
-      {/* </div> */}
     </Router>
   );
 }
